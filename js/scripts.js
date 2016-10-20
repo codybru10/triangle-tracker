3d21558a6a4010487d61a5eea1f293f45c0bdea0
Cody Brubaker and Jim Padilla <cody@>
2016-10-20 13:57:20 -0700

    Separated business and interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
 
-var triangleMath = function() {
-  var sideOne = parseInt($("#side1").val());
-  var sideTwo = parseInt($("#side2").val());
-  var sideThree = parseInt($("#side3").val());
+var triangleMath = function(sideOne, sideTwo, sideThree) {
 
   if (sideOne === sideTwo && sideTwo === sideThree) {
-    $(".output").text("This is an Equilateral Triangle");
+    return "This is an Equilateral Triangle";
   } else if ((sideOne === sideTwo && sideOne + sideTwo >= sideThree) || (sideTwo === sideThree && sideTwo + sideThree >= sideOne) || (sideOne === sideThree && sideOne + sideThree >= sideTwo) && !(sideOne + sideTwo <= sideThree) || (sideOne + sideThree <= sideTwo) || (sideTwo + sideThree <= sideOne)) {
-    $(".output").text("This is an Isosceles Triangle");
+    return "This is an Isosceles Triangle";
   } else if ((sideOne != sideTwo && sideOne != sideThree && sideTwo != sideThree) && !(sideOne + sideTwo <= sideThree) || (sideOne + sideThree <= sideTwo) || (sideTwo + sideThree <= sideOne)) {
-    $(".output").text("This is a Scalene Triangle");
-  } else if ((sideOne + sideTwo <= sideThree) || (sideOne + sideThree <= sideTwo) || (sideTwo + sideThree <= sideOne)){
-    $(".output").text("This is not a triangle");
+    return "This is a Scalene Triangle";
+  } else if ((sideOne + sideTwo <= sideThree) || (sideOne + sideThree <= sideTwo) || (sideTwo + sideThree <= sideOne)) {
+    return "This is not a triangle";
+  } else {
+    alert("Error");
   }
 }
 
@@ -19,7 +18,13 @@ var triangleMath = function() {
 $(document).ready(function() {
 
   $("form").submit(function(event){
-    triangleMath();
+    var sideOne = parseInt($("#side1").val());
+    var sideTwo = parseInt($("#side2").val());
+    var sideThree = parseInt($("#side3").val());
+
+    var answer = triangleMath(sideOne, sideTwo, sideThree);
+
+    $(".output").text(answer);
 
     event.preventDefault();
   });
